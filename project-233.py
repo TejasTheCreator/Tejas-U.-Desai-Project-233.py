c1a1fc1b2dbf43b97c1d0d14b6427a2248e8e924
Tejas U. Desai <71921949+TejasTheCreator@users.noreply.github.com>
2022-04-09 12:02:25 +0530

    Add files via upload

---
+++ project-233.py
@@ -0,0 +1,7 @@
+import pandas as pd
+df = pd.read_csv('projectC233_studentData')
+df[Total_marks_obtained] = df.iloc[:, [2, 3, 4]].sum(axis=1)
+df.loc[df['Total_marks_obtained'] > 250, 'Grade'] = 'A'
+df.loc[df['Total_marks_obtained'] < 250, 'Grade'] = 'B'
+df.loc[df['Percentage'] = df['Total_marks_obtained'] / df['Overall_Total'] * 100 
+print(df)
